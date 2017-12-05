6d883d38ff3d1b4f373c4f451f518e1b8a22e94f
ppatel118 <31360559+ppatel118@users.noreply.github.com>
2017-12-05 15:44:50 -0600

    Update database.js

--- login/config/database.js
+++ login/config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
 
-	'url' : 'mongodb://poojapatel:Jaijalram#18@travelnow-shard-00-00-rmibq.mongodb.net:27017'
-
+	'url' : 'mongodb://poojapatel1808:Jaijalaram#18@travelnow-shard-00-00-rmibq.mongodb.net:27017,travelnow-shard-00-01-rmibq.mongodb.net:27017,travelnow-shard-00-02-rmibq.mongodb.net:27017/test?ssl=true&replicaSet=TravelNow-shard-0&authSource=admin'
 };

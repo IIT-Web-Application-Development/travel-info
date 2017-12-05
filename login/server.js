0b17ddd54eb3670360707e3023666471fc4c1a82
ppatel118 <31360559+ppatel118@users.noreply.github.com>
2017-12-05 16:30:31 -0600

    Update server.js

--- login/server.js
+++ login/server.js
@@ -12,7 +12,7 @@ var session = require('express-session');
 
 var connect = require('connect');
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://poojapatel1808:Jaijalaram#18@travelnow-shard-00-00-rmibq.mongodb.net:27017,travelnow-shard-00-01-rmibq.mongodb.net:27017,travelnow-shard-00-02-rmibq.mongodb.net:27017/test?ssl=true&replicaSet=TravelNow-shard-0&authSource=admin';
+var url = 'mongodb://poojapatel1808:Jaijalaram#18@travelnow-shard-00-00-rmibq.mongodb.net:27017/test?ssl=true&replicaSet=TravelNow-shard-0&authSource=admin';
 
 var configDB = require('./config/database.js');
 
